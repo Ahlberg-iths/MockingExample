86d59d31be00045e46f830755a63ae1ac60c7e00
Ahlberg-iths <113943951+Ahlberg-iths@users.noreply.github.com>
2023-02-04 22:40:10 +0100

    Add test nr2

--- src/main/java/com/assignment2/Game.java
+++ src/main/java/com/assignment2/Game.java
@@ -4,6 +4,7 @@ public class Game {
     int score = 0;
 
     void roll (int pins) {
+        score = 9;
     }
 
     int score() {
--- src/test/java/com/assignment2/GameTest.java
+++ src/test/java/com/assignment2/GameTest.java
@@ -12,4 +12,13 @@ class GameTest {
 
         assertEquals(0, game.score());
     }
+
+    @Test
+    void scoreShouldReturn9WhenOneRollWith9PinsHasBeenMade() {
+        var game = new Game();
+
+        game.roll(9);
+
+        assertEquals(9, game.score());
+    }
 }
