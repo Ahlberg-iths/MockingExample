9b6e57ca0935cc4588c3c9b802df5deb01dcdfac
Ahlberg-iths <113943951+Ahlberg-iths@users.noreply.github.com>
2023-05-26 19:39:18 +0200

    Add test nr10

--- src/main/java/com/assignment2/Game.java
+++ src/main/java/com/assignment2/Game.java
@@ -7,12 +7,43 @@ public class Game {
     int bonusPoints = 0;
     boolean firstRollInFrame = true;
     int firstRoll = 0;
+    int frame = 1;
+    boolean gameIsComplete = false;
+    Integer specialRoll1 = null;
+    Integer specialRoll2 = null;
 
     void roll (int pins) {
+        if (gameIsComplete) return;
+        if (frame == 10) {
+            if (specialRoll1 == null) {
+                specialRoll1 = pins;
+            } else if (specialRoll2 == null) {
+                specialRoll2 = pins;
+                if (specialRoll1 == 10) {
+                    if (doubleBonus) bonusPoints += pins;
+                    bonusPoints += pins;
+                    return;
+                } else if (specialRoll1 + pins == 10) {
+                    if (bonusRounds > 0) bonusPoints += pins;
+                    score += pins;
+                    return;
+                } else {
+                    if (bonusRounds > 0) bonusPoints += pins;
+                    score += pins;
+                    gameIsComplete = true;
+                    return;
+                }
+            } else {
+                bonusPoints += pins;
+                gameIsComplete = true;
+                return;
+            }
+        }
         if (pins < 0 || pins > 10) handleMachineError();
         if (bonusRounds > 0) bonusPoints+= pins;
         if (doubleBonus) bonusPoints+= pins;
-
+        bonusRounds = Math.max(0, bonusRounds - 1);
+        doubleBonus = false;
         if (firstRollInFrame) handleFirstThrow(pins);
         else handleSecondThrow(pins);
         score += pins;
@@ -26,15 +57,19 @@ public class Game {
         if (pins != 10) {
             firstRoll = pins;
             firstRollInFrame = false;
+            return;
         }
-        else if (bonusRounds > 0) doubleBonus = true;
-        else bonusRounds = 2;
+        if (bonusRounds > 0) doubleBonus = true;
+        bonusRounds = 2;
+        if (frame != 10) frame++;
     }
 
     private void handleSecondThrow(int pins) {
         if (pins + firstRoll > 10) handleMachineError();
         if (pins + firstRoll == 10) bonusRounds = 1;
         firstRoll = 0;
+        frame++;
+        firstRollInFrame = true;
     }
 
     int score() {
--- src/test/java/com/assignment2/GameTest.java
+++ src/test/java/com/assignment2/GameTest.java
@@ -3,7 +3,12 @@ package com.assignment2;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -86,5 +91,23 @@ class GameTest {
         assertEquals(60, game.score());
     }
 
+    @ParameterizedTest
+    @MethodSource("fullGameWithScore")
+    void aCompleteGameShouldReturnTheCorrectScore(int expectedScore, List<Integer> allRollsOfCompleteGame) {
+        allRollsOfCompleteGame.forEach(roll -> game.roll(roll));
 
+        assertEquals(expectedScore, game.score());
+    }
+
+    static Stream<Arguments> fullGameWithScore() {
+        return Stream.of(
+                Arguments.of(0, List.of(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0)),
+                Arguments.of(190, List.of(8,2,9,1,10,7,3,10,9,1,8,2,10,6,3,10,10,5)),
+                Arguments.of(85, List.of(9,0,5,3,8,1,1,7,5,3,4,5,10,8,0,8,0,0,0)),
+                Arguments.of(300, List.of(10,10,10,10,10,10,10,10,10,10,10,10)),
+                Arguments.of(107, List.of(8,1,7,0,3,6,8,0,8,2,3,4,10,8,2,8,0,8,0)),
+                Arguments.of(163, List.of(10,9,1,7,2,8,1,10,8,2,10,7,3,10,9,0)),
+                Arguments.of(245, List.of(6,4,10,10,10,10,10,10,7,3,8,2,10,9,1))
+        );
+    }
 }
